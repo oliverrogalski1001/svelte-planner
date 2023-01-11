773ececa50d6cfcd74c95e1bf28c176ca643a3d3
gamatatsu <33262512+gamatatsu@users.noreply.github.com>
2023-01-11 13:43:39 -0600

    carbon components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	import "../app.css"
+	import "carbon-components-svelte/css/all.css"
+
+
 </script>
 
-<div class='text-center font-mono p-4'>
-	<h1 class='text-3xl'>UIUC Course Planner</h1>
-	<button class=''>Add Class</button>
-</div>
+<h1>UIUC Class Planner</h1>
+<button>Hello</button>

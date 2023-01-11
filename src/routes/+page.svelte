45931e98c6854796df3c8ca1dfb708513b4f6e9a
Oliver Rogalski <33262512+oliverrogalski1001@users.noreply.github.com>
2023-01-11 15:19:31 -0600

    semester columns

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script>
 	import "../app.css"
 	import "carbon-components-svelte/css/all.css"
+	import Semester from "./Semester.svelte"
 
-
+	let semesters = Array.from(Array(8).keys()).map(i => i + 1);
 </script>
 
-<h1>UIUC Class Planner</h1>
-<button>Hello</button>
+<div class='text-center'>
+	<h1 class='p-4'>UIUC Class Planner</h1>
+	<div class='flex flex-row'>
+		{#each semesters as semester}
+			<Semester num={semester} />
+		{/each}
+	</div>
+</div>
---
+++ src/routes/Semester.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { Button, Modal, Select, SelectItem } from 'carbon-components-svelte';
+
+	export let num
+	let open = false
+</script>
+
+<div>
+	<h2>Semester {num}</h2>
+	<Button on:click={() => open = true}>Add Class</Button>
+	<Modal
+		bind:open
+		modalHeading='Add Class'
+		primaryButtonText="Add"
+		secondaryButtonText="Cancel"
+		on:click:button--primary={() => open = false}
+		on:click:button--secondary={() => open = false}
+	>
+		<div class='flex flex-row justify-around'>
+			<div>
+				<p>Subject</p>
+				<Select>
+					<SelectItem value="STAT"/>
+				</Select>
+			</div>
+			<div>
+				<p>Number</p>
+				<Select>
+					<SelectItem value="400"/>
+				</Select>
+			</div>
+		</div>
+	</Modal>
+</div>
